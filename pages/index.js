1164102b9ee257a9cbdd632c208a381836956a10
perlik11s <simon.perlik@gmail.com>
2025-07-08 18:40:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function () {
   return (
     <div style={{ 
       display: "flex", 
@@ -15,7 +15,7 @@ export default function Home() {
 }
 import Link from 'next/link';
 
-export default function Home() {
+export default function () {
   return (
     <div style={{
       display: 'flex',
